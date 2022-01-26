b20726d6c7691955332fa636f8b9ca5cb14e6414
pauloserra81 <97617344+pauloserra81@users.noreply.github.com>
2022-01-26 21:35:10 +0000

    Update m7parser.ino

--- m7parser/m7parser.ino
+++ m7parser/m7parser.ino
@@ -1,7 +1,7 @@
 /*
    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleScan.cpp
    Ported to Arduino ESP32 by Evandro Copercini
-   Changed to a beacon scanner to report iBeacon, EddystoneURL and EddystoneTLM beacons by beegee-tokyo
+   Changed to connect to a Keiser M7 structure by Paulo Serra
 */
 #include <esp_now.h>
 #include <WiFi.h>
